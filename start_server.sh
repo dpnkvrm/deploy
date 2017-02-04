1f4972c3f912cb09758fb915e166df0a74c514b3
Deepank Verma <deepank.verma@HC15-030.local>
2017-02-04 17:17:10 +0530

    updated start script

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,5 @@
 cd /var/www/reshotel;
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -L https://get.rvm.io | bash -s stable;
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \curl -L https://get.rvm.io | bash -s stable;
 source /home/deploy/.rvm/scripts/rvm;
 rvm autolibs disable;
 rvm requirements;
